544bee706c89548563157f1c7786ee1ac1ae4bb9
Rohini Deshmukh <121260777+RohiniDeshmukh@users.noreply.github.com>
2024-06-20 13:25:18 -0400

    Time: 13 ms (65.42%) | Memory: 11.7 MB (53.64%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -5,17 +5,14 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
+       
         n1=len(word1)
         n2=len(word2)
         merged=""
-
         for i in range (0,min(n1,n2)):
-
             merged=merged+word1[i]+word2[i]
-
         if n1>n2:
             merged=merged+word1[n2:]
         else:
             merged=merged+word2[n1:]
-
         return merged
